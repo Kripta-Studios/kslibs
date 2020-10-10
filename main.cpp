0c879d75278ce88e8f29b4a75df7a647c5683e79
Kripta-Studios <68393696+Kripta-Studios@users.noreply.github.com>
2020-10-10 17:53:27 +0200

    Adding explanation

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Created the 10/10/2020\n";
+    std::cout << "Right now the idea is premature, \n" 
+              << "this will be change in a few days or weeks" << std::endl;
+
+    return 0;
+} 
