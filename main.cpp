9d74c3c74ac3b23ed35f0254c4eaecfab76b96a1
Kripta-Studios <68393696+Kripta-Studios@users.noreply.github.com>
2021-02-13 16:44:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     std::cout << "Created the 10/10/2020\n";
     std::cout << "Right now the idea is premature, \n" 
-              << "this will be change in a few days or weeks" << std::endl;
+              << "this will be change in a few days or weeks" << "\n";
 
     return 0;
 } 
