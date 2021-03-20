9999bcf3d54030c2dac91c321d32ba32577ca7c6
Kripta-Studios <kriptastudios@tutamail.com>
2021-03-20 11:18:07 +0100

    adfsdf

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() 
 {
     std::cout << "Created the 10/10/2020\n";
-    std::cout << "Right now the idea is premature\n";
+    std::cout << "Right now the idea is premature.\n";
 
     return 0;
 } 
